8ea4b2e3bf7b34859013f35b4806b6113294e499
Yuliya <kuroi.kasa1509@gmail.com>
2019-08-10 00:31:06 +0700

    Add basic components

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() { 
+        return ( 
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>contacts</li>
+                </ul>
+            </nav>
+         );
+    }
+}
+ 
+export default Navbar ;
