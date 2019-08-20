a47d49f0f3453d5f71d55616436b7141cad11a20
Yuliya <kuroi.kasa1509@gmail.com>
2019-08-20 23:28:31 +0700

    Add hooks to use with Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    static contextType = ThemeContext;
-    render() { 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-        return ( 
-            <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style={{background: theme.ui}}></li>
-                    <li style={{background: theme.ui}}></li>
-                    <li style={{background: theme.ui}}></li>
-                </ul>
-            </div>
-         );
-    }
+
+const BookList = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (
+        <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+        <ul>
+            <li style={{background: theme.ui}}></li>
+            <li style={{background: theme.ui}}></li>
+            <li style={{background: theme.ui}}></li>
+        </ul>
+    </div>
+    );
 }
- 
+
+
 export default BookList;
